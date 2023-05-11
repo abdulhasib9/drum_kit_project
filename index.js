042cfb28bf029d5039e1ed6f4da609bb6a2354b1
abdul hasib yousufzai <yousufzai.abdulhasib@gmail.com>
2023-05-10 23:52:26 -0700

    added css and js code for functionality and design

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
-function handleClick(){
-    alert("i got clicked")
+// document.querySelector("button").addEventListener("click",handleClick)
+// function handleClick(){
+//     alert("i got clicked")
+// }
+
+for( let i =0 ; i <document.querySelectorAll(".drum").length;i++){
+    document.querySelectorAll("button")[i].addEventListener("click", function () {
+        // if (document.querySelectorAll(".drum")[i].innerHTML=="w"){
+        //     let audio = new Audio("sounds/crash.mp3")
+        // audio.play()
+        // }
+       
+
+        let htmlButtonInnerText = this.innerHTML;
+        switch (htmlButtonInnerText) {
+            case "w":
+                let crash = new Audio("sounds/crash.mp3")
+                audio.play()
+                break;
+            case "w":
+                let kick_bass= new Audio("sounds/tom-3.mp3")
+                audio.play()
+                break;
+        
+            default:
+                break;
+        }
+    })
+    
 }
