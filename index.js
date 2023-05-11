881e216cad240a0c11eafc221bcdefc5ea2e4578
abdul hasib yousufzai <yousufzai.abdulhasib@gmail.com>
2023-05-11 00:21:41 -0700

    added code for keypress event and enhanced the existing code

--- index.js
+++ index.js
@@ -3,48 +3,92 @@
 //     alert("i got clicked")
 // }
 
-for( let i =0 ; i <document.querySelectorAll(".drum").length;i++){
-    document.querySelectorAll("button")[i].addEventListener("click", function () {
-        // if (document.querySelectorAll(".drum")[i].innerHTML=="w"){
-        //     let audio = new Audio("sounds/crash.mp3")
-        // audio.play()
-        // }
-       
+for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
+  document.querySelectorAll("button")[i].addEventListener("click", function () {
+    // if (document.querySelectorAll(".drum")[i].innerHTML=="w"){
+    //     let audio = new Audio("sounds/crash.mp3")
+    // audio.play()
+    // }
 
-        let htmlButtonInnerText = this.innerHTML;
-        switch (htmlButtonInnerText) {
-            case "w":
-                let crash = new Audio("sounds/crash.mp3")
-                crash.play()
-                break;
-            case "a":
-                let kick_bass= new Audio("sounds/kick-bass.mp3")
-                kick_bass.play()
-                break;
-             case "s":
-                    let snare= new Audio("sounds/snare.mp3")
-                    snare.play()
-                    break;
+    let htmlButtonInnerText = this.innerHTML;
+    playSound(htmlButtonInnerText)
 
-                case "d":
-                    let tom1= new Audio("sounds/tom-1.mp3")
-                    tom1.play()
-                    break;
-             case "j":
-                        let tom2= new Audio("sounds/tom-2.mp3")
-                        tom2.play()
-                        break;
-             case "k":
-                            let tom3= new Audio("sounds/tom-3.mp3")
-                            tom3.play()
-                            break;
-                            case "l":
-                                let tom4= new Audio("sounds/tom-4.mp3")
-                                tom4.play()
-                                break;
-            default:
-                break;
-        }
-    })
-    
+
+    //enhanced this part of the code with a fucntion
+    // switch (htmlButtonInnerText) {
+    //   case "w":
+    //     let crash = new Audio("sounds/crash.mp3");
+    //     crash.play();
+    //     break;
+    //   case "a":
+    //     let kick_bass = new Audio("sounds/kick-bass.mp3");
+    //     kick_bass.play();
+    //     break;
+    //   case "s":
+    //     let snare = new Audio("sounds/snare.mp3");
+    //     snare.play();
+    //     break;
+
+    //   case "d":
+    //     let tom1 = new Audio("sounds/tom-1.mp3");
+    //     tom1.play();
+    //     break;
+    //   case "j":
+    //     let tom2 = new Audio("sounds/tom-2.mp3");
+    //     tom2.play();
+    //     break;
+    //   case "k":
+    //     let tom3 = new Audio("sounds/tom-3.mp3");
+    //     tom3.play();
+    //     break;
+    //   case "l":
+    //     let tom4 = new Audio("sounds/tom-4.mp3");
+    //     tom4.play();
+    //     break;
+    //   default:
+    //     break;
+    // }
+  })
+
 }
+
+function playSound(key){
+    switch (key) {
+        case "w":
+          let crash = new Audio("sounds/crash.mp3");
+          crash.play();
+          break;
+        case "a":
+          let kick_bass = new Audio("sounds/kick-bass.mp3");
+          kick_bass.play();
+          break;
+        case "s":
+          let snare = new Audio("sounds/snare.mp3");
+          snare.play();
+          break;
+  
+        case "d":
+          let tom1 = new Audio("sounds/tom-1.mp3");
+          tom1.play();
+          break;
+        case "j":
+          let tom2 = new Audio("sounds/tom-2.mp3");
+          tom2.play();
+          break;
+        case "k":
+          let tom3 = new Audio("sounds/tom-3.mp3");
+          tom3.play();
+          break;
+        case "l":
+          let tom4 = new Audio("sounds/tom-4.mp3");
+          tom4.play();
+          break;
+        default:
+          break;
+      }
+}
+
+//code for the keypress event and making sound for the coresponding button
+document.addEventListener("keypress",function(e){
+    playSound(e.key)
+})
